82a66c9252e51ed928402c209f701e69f75041b5
DariaLuca <andreeadarialuca@gmail.com>
2024-04-01 19:36:40 +0300

    Show next question

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ class Quiz {
         this.answerButtons = document.getElementById('answer-buttons');
         this.feedback = document.getElementById('feedback');
         this.nextButton = document.getElementById('next-button');
-        this.showQuestion(this.questions[0]);
+        this.currentQuestionIndex = 0;
+        this.showQuestion(this.questions[this.currentQuestionIndex]);
+        this.nextButton.addEventListener('click', () => this.nextQuestion());
     }
 
 
@@ -30,6 +32,7 @@ class Quiz {
             this.nextButton.classList.remove('hide');
             this.feedback.textContent = question.explanation;
             button.classList.add('correct');
+            this.feedback.classList.remove('text-wrong');
             this.feedback.classList.add('text-correct');
         } else {
             button.classList.add('wrong');
@@ -37,7 +40,24 @@ class Quiz {
             this.feedback.classList.add('text-wrong');
         }
     }
-}
+
+    nextQuestion() {
+        this.currentQuestionIndex++;
+        this.resetQuestion();
+        if(this.currentQuestionIndex >= this.questions.length) {
+            console.log('Sfarsit Quiz');
+        }else {
+            this.showQuestion(this.questions[this.currentQuestionIndex]);
+        }
+    }
+
+    resetQuestion() {
+        this.feedback.classList.remove('text-correct', 'text-wrong');
+        this.feedback.innerHTML = '';
+        this.nextButton.classList.add('hide');
+    }
+ }
+
 
 async function initQuiz() {
     const fileContent = await fetch('./questions.json');
